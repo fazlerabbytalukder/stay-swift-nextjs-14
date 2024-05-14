025dbc900519c4eefc1bb1edfdffa224cbd0993c
Fazle Rabby Talukder <talukderfazlerabby@gmail.com>
2024-05-14 21:03:47 +0600

    remove facebook

--- components/auth/SocialLogins.jsx
+++ components/auth/SocialLogins.jsx
@@ -9,21 +9,21 @@ const SocialLogins = ({ mode }) => {
     signIn("google", { callbackUrl: 'http://localhost:3000/bookings' });
   }
 
-  const handleFacebookAuth = (event) => {
-    signIn("facebook", { callbackUrl: 'http://localhost:3000/bookings' });
-  }
+  // const handleFacebookAuth = (event) => {
+  //   signIn("facebook", { callbackUrl: 'https://stay-swift-nextjs-14.vercel.app/bookings' });
+  // }
   return (
     <>
       <div className="text-center text-xs text-gray-500">
         {mode === 'register' ? (<Link className="underline" href="/login">Login</Link>) : (<Link className="underline" href="/register">Register</Link>)} or Signup with
       </div>
       <div className="flex gap-4">
-        <button
+        {/* <button
           onClick={handleFacebookAuth}
           className=" w-full mt-4 py-2 border-gray-600/30 border rounded-md flex items-center gap-2 justify-center">
           <Image src="/fb.png" alt="facebook" width={40} height={40} />
           <span>Facebook</span>
-        </button>
+        </button> */}
         <button
           onClick={handleAuth}
           className=" w-full mt-4 py-2 border-gray-600/30 border rounded-md flex items-center gap-2 justify-center">
