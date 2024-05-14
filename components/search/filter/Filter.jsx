e963ee7dbf6a41bc7201fe5d15ebe05401110dfb
Fazle Rabby Talukder <talukderfazlerabby@gmail.com>
2024-05-14 19:42:07 +0600

    stay swift

---
+++ components/search/filter/Filter.jsx
@@ -0,0 +1,19 @@
+import FilterByStarCategory from "./FilterByStarCategory";
+
+const Filter = () => {
+  return (
+    <>
+      <div className="col-span-3 space-y-4">
+        {/* <SortHotel /> */}
+
+        {/* <FilterByPriceRange /> */}
+
+        <FilterByStarCategory />
+
+        {/* <FilterByAmenities /> */}
+      </div>
+    </>
+  );
+};
+
+export default Filter;
